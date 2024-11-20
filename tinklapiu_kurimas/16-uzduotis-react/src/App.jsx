f78d94cbac515d8a31542eee560e76b3cf496edc
KarolisRZN <qewmnbp@gmail.com>
2024-11-20 13:36:51 +0200

    complete 16-uzduotis-react

---
+++ tinklapiu_kurimas/16-uzduotis-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PostsList from "./components/PostsList";
+
+  function App() {
+    return (
+      <div>
+        <PostsList />
+      </div>
+    );
+  }
+  
+  export default App;
---
+++ tinklapiu_kurimas/16-uzduotis-react/src/components/PostContent.jsx
@@ -0,0 +1,33 @@
+// export default function PostContent({ title, content, img }) {
+    // return (
+        // <div>
+            // <h2>{title}</h2>
+            // <img 
+            // src={img}
+            // alt={title}
+        // />
+        // <p>{content}</p>
+        // </div>
+    // );
+// }
+
+export default function PostContent({
+    title,
+    content, 
+    img,
+    setLearnt,
+    id,
+    status,
+}) {
+    return (
+        <div className="col-4">
+            <h2>{title}</h2>
+            <img src={img}
+            alt={title}
+            />
+            <p>{content}</p>
+            <p>{status ? "Ismokau" : "Mokausi"}</p>
+            <button onClick={() => setLearnt(id)}>OK</button>
+        </div>
+    );
+}
